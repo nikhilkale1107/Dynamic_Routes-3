7bc0c33cd6fcc3096c69057ef92df2e6d9a3a0fb
Nikhil Kale <nikhilkale1107@gmail.com>
2024-03-05 13:19:26 +0530

    Use of Sequelize

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
-const db = require('./util/database');
+// const db = require('./util/database');
+
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -15,7 +17,13 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-
+// db.execute('SELECT * FROM products')
+// .then(result => {
+//     console.log(result[0], result[1])
+// })
+// .catch(() => {
+//     console.log(err);
+// });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -25,4 +33,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync()
+.then(result => {
+    // console.log(result);
+    app.listen(3000);
+})
+.catch((err) => {
+    console.log(err)
+});
+
--- util/database.js
+++ util/database.js
@@ -1,10 +1,19 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Nikhil@1107'
-})
+// const pool = mysql.createPool({
+//     host:'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: 'Nikhil@1107'
+// })
 
-module.exports = pool.promise();
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete','root','Nikhil@1107',{
+    dialect: 'mysql',
+    host:'localhost'
+});
+
+module.exports = sequelize;
